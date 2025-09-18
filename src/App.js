8724c37bbd9e08b0d9aae27265fe1ef335af0f17
mgc7balamurugann24 <mgc7balamurugann24@gmail.com>
2025-09-18 14:12:20 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import CategoryPage from './pages/CategoryPage';
-import NewsPage from './pages/NewsPage';
-import NavbarComponent from './components/NavbarComponent';
-import Footer from './components/Footer';
 
-function App() {
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+import AddProduct from './components/AddProduct';
+import ProductCatalog from './components/Product/ProductCatalog';
+import Layout from './Layout';
+import Cart from './components/Cart/Cart';
+import NotFound from './pages/NotFound';
+import { CartProvider } from './context/CartContext';
+import Inventory from './components/Inventory/Inventory';
+import { InventoryProvider } from './context/InventoryContext';
+import { SalesProvider } from './context/SalesContext';
+import Sales from './components/Sales/Sales';
+
+const router = createBrowserRouter([
+  {
+    element: (
+      <Layout />
+    ),
+    children: [
+      {
+        path: "/",
+        element: <ProductCatalog />,
+      },
+      {
+        path: "/add-product",
+        element: <AddProduct />,
+      },
+      {
+        path: "/inventory",
+        element: <Inventory />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/sales",
+        element: <Sales />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ]
+  }
+])
+
+const App = () => {
   return (
-    <div className="App">
-        <NavbarComponent />
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/category/:id" element={<CategoryPage/>} />
-          <Route exact path="/news/:id" element={<NewsPage/>} />
-        </Routes>
-        <Footer />
-    </div>
+    <InventoryProvider>
+        <CartProvider>
+          <SalesProvider>
+            <RouterProvider router={router} />
+          </SalesProvider>
+        </CartProvider>
+    </InventoryProvider>
   );
 }
 
-export default App;
+export default App
